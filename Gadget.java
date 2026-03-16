f4715126b45787e933367be2c9ef0485d1b9be68
blehsr <blesss428@gmail.com>
2026-03-16 13:45:28 +0000

    Initial commit - Complete Gadget Shop project with all files

--- Gadget.java
+++ Gadget.java
@@ -1,7 +1,20 @@
 public class Gadget {
-    private final String name;
-    public Gadget(String name) { this.name = name; }
-    public String getName() { return name; }
-    @Override public String toString() { return "Gadget{name='" + name +"'}"; }
-    public void display() { System.out.println("name=" + name);}
+    
+// this is for the text fields (basic aspects of the gadget)
+    private final String model;
+    private final double price;
+    private final int weight;
+    private final String size;
+    
+    
+    public Gadget(String model, double price, int weight, String size) { this.model = model; this.price = price; this.weight = weight; this.size = size; }
+    public String getModel() { return model; }
+    public double getPrice() {return price;}
+    public 
+    
+    
+    
+    
+    @Override public String toString() { return "Gadget{name='" + model +"'}"; }
+    public void display() { System.out.println("name=" + model);}
 }
--- GadgetShop.java
+++ GadgetShop.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import java.util.ArrayList;
 
+// github test
 public class GadgetShop extends Application {
 // all the variables (refer back)
     private final TextField nameField = new TextField();
