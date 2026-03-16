217f672aea62ae6e9707706a99e9220cbf5c1d10
blehsr <blesss428@gmail.com>
2026-03-16 14:30:07 +0000

    Added changes to gadget class

--- Gadget.java
+++ Gadget.java
@@ -10,11 +10,13 @@ public class Gadget {
     public Gadget(String model, double price, int weight, String size) { this.model = model; this.price = price; this.weight = weight; this.size = size; }
     public String getModel() { return model; }
     public double getPrice() {return price;}
-    public 
+    public int getWeight() {return weight;}
+    public String getSize() {return size;}
     
     
     
     
-    @Override public String toString() { return "Gadget{name='" + model +"'}"; }
-    public void display() { System.out.println("name=" + model);}
+    @Override 
+    public String toString() { return "Gadget{model='" + model +",, price = £" + price + ",weight=" + weight + "g, size=" + size + " }"; }
+    public void display() { System.out.println("model=" + model); System.out.println("Price: £" + price); System.out.println("Weight:" + weight +"g"); System.out.println("Size:" + size);}                                
 }
