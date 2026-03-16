6b284ce1d20b4ee70848be0e87f81de1b00568ab
blehsr <blesss428@gmail.com>
2026-03-16 15:12:29 +0000

    Changes to MP3 and Smartphone / Added the model, price, weight and size

--- MP3.java
+++ MP3.java
@@ -1,17 +1,26 @@
 public class MP3 extends Gadget {
     private int memory;
-    public MP3(String name, int memory) { 
-        super(name); 
+    public MP3(String model, int memory, double price, int weight, String size) { 
+        super(model, price, weight, size); 
         this.memory=memory;
     }
     
+    public int getMemory() { return memory; }
+    
     public void moreMemory(int amount) {
+        if (amount > 0) {
+            memory += amount;
+            System.out.println("Added" + amount + "MB memory. Total memory:" + memory + "MB");
+        } else { System.out.println("Error: Please enter a positive amount of memory.");
+        }
     }
+        
 
-    @Override public String toString() { return "MP3 version{name='" + getName() + "' | egg='"+memory+"'}"; }
+    @Override public String toString() { return "MP3 version{model='" + getModel() + "' | price=£'"+ getPrice() + "| weight =" + getWeight() + "| g, size =" + getSize() + "| memory =" + memory + "MB}"; }
+    
     @Override public void display() { 
         System.out.println("MP3");
         super.display();
-        System.out.println("memory=" + memory);
+        System.out.println("memory=" + memory + "MB");
     }
 }
--- Smartphone.java
+++ Smartphone.java
@@ -1,4 +1,5 @@
 public class Smartphone extends Gadget {
-    public Smartphone(String name) { super(name); }
-    @Override public String toString() { return "Smartphone brand{name='" + getName() + "'}"; }
+    
+    public Smartphone(String model, double price, int weight, String size) { super(model, price, weight, size); }
+    @Override public String toString() { return "Smartphone brand{model='" + getModel() + "price = £" + getPrice() + ",weight=" + getWeight() + "g, size =" + getSize(); }
 }
